78bcf6d6bebdfd5b08c64fa15589f2cc9c885b35
Amin Movahedi <amin1384movahedi@zohomail.com>
2023-11-23 13:04:19 -0600

    adding some comments and information into the code

--- DoublyLinkedList/DoublyLinkedList_Class.py
+++ DoublyLinkedList/DoublyLinkedList_Class.py
@@ -1,9 +1,13 @@
+# This is the Doubly Linked List Data Structure module
+
+# Build the Node Constractor
 class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
         self.prev = None
 
+# Implement the DoublyLinkedList
 class DoublyLinkedList:
     def __init__(self):
         self.head = None
@@ -17,6 +21,7 @@ class DoublyLinkedList:
             print(temp.value)
             temp = temp.next
 
+    # adding a new node into the last of the list
     def append(self, value):
         new_node = Node(value)
 
@@ -33,7 +38,9 @@ class DoublyLinkedList:
 
         return True
     
+    # removing the last node from the list
     def pop(self):
+        # if the length of the list is zero, we have nothing to pop
         if self.length == 0:
             return None
         
@@ -51,6 +58,7 @@ class DoublyLinkedList:
 
         return temp
     
+    # adding a new node into the first of the list
     def prepend(self, value):
         new_node = Node(value)
         if self.length == 0:
@@ -66,7 +74,9 @@ class DoublyLinkedList:
 
         return True
     
+    # removing the first item of the list
     def pop_first(self):
+        # if the length of the list is zero, we have nothing to pop
         if self.length == 0:
             return None
         
@@ -84,10 +94,13 @@ class DoublyLinkedList:
 
         return temp
 
+    # build a function to get a index and return the value of that index
     def get(self, index):
+        # if the entered index number was out of range, return None
         if index < 0 or index >= self.length:
             return None
         
+        # if the entered index number is less than the middle of the list, start the loop from head, othervise, start from that tail to the head.
         temp = self.head
         if index < (self.length / 2):
             for _ in range(index):
@@ -100,6 +113,7 @@ class DoublyLinkedList:
 
         return temp
     
+    # setting a value into a existing node by index
     def set_value(self, index, value):
         temp = self.get(index)
         if temp:
@@ -108,6 +122,10 @@ class DoublyLinkedList:
         
         return False
     
+    # createing a method to insert a new node into our list
+    # if the entered number of index was outta the valid range, just return False
+    # if the num of index is one, use the prepend function that we built that before
+    # also if the number of index was equal to the length of the list, just call the append method
     def insert(self, index, value):
         if index < 0 or index > self.length:
             return False
@@ -131,13 +149,17 @@ class DoublyLinkedList:
 
         return True
     
+    # this is a method to remove a node by a given index
     def remove(self, index):
+        # if the index number is in the invalid range of numbers, just return None
         if index < 0 or index >= self.length:
             return None
         
+        # and if the index is equal to the zero just use the pop_first method which we build that before
         if index == 0:
             return self.pop_first()
         
+        # also if the number of index was equal to the number of length minus one, use the pop function
         if index == (self.length - 1):
             return self.pop()
         
@@ -152,9 +174,11 @@ class DoublyLinkedList:
 
         return temp
     
+    # just a simple method to return the number of length of the list
     def len(self):
         return self.length
     
+    # removing a new using a value instead of the index number
     def remove_value(self, value):
         index = 0
         temp = self.head
